22a677cc586e30bae5ecf943a17b7aa74593f276
Winnguyen <khoanguyen1507vn@gmail.com>
2020-06-15 15:00:27 +0700

    Init repos

---
+++ gitcmd.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+#Git cmd to create repository
+user="Winnguyen1511"
+api=https://api.github.com/user/repos
+reponame="Internship"
+private="false"
+gitdir=git@github.com:$user/$reponame.git
+curl -u $user $api -d '{"name":"Internship"}'
+
+git init
+git add .
+git commit -m "Init repos"
+git remote add origin $gitdir
+git push -u origin master
