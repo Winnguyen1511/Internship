8e77a5b29759fe6d15c3ec46dca4ecfb208bf148
Winnguyen <khoanguyen1507vn@gmail.com>
2020-06-25 19:50:38 +0700

    finish Templates

--- June/DjangoLearning/django_project/blog/views.py
+++ June/DjangoLearning/django_project/blog/views.py
@@ -2,8 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Winnguyen1511', 
+        'title': 'Blog Post 1', 
+        'content': 'Fisrt post content', 
+        'date_posted':'August 27, 2020'
+    },
+    {
+        'author': 'Hiennguyen', 
+        'title': 'Blog Post 2', 
+        'content': 'Second post content', 
+        'date_posted':'August 28, 2020'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/index.html')
+    context = {
+        'posts': posts
+    }
+    #Pass data to the render.
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
