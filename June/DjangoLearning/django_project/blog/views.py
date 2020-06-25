012cbd8fe70689c078751e5aaa54365c23021c54
Winnguyen <khoanguyen1507vn@gmail.com>
2020-06-25 20:51:18 +0700

    ORM django

--- June/DjangoLearning/django_project/blog/views.py
+++ June/DjangoLearning/django_project/blog/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Winnguyen1511', 
-        'title': 'Blog Post 1', 
-        'content': 'Fisrt post content', 
-        'date_posted':'August 27, 2020'
-    },
-    {
-        'author': 'Hiennguyen', 
-        'title': 'Blog Post 2', 
-        'content': 'Second post content', 
-        'date_posted':'August 28, 2020'
-    }
-]
+# posts = [
+#     {
+#         'author': 'Winnguyen1511', 
+#         'title': 'Blog Post 1', 
+#         'content': 'Fisrt post content', 
+#         'date_posted':'August 27, 2020'
+#     },
+#     {
+#         'author': 'Hiennguyen', 
+#         'title': 'Blog Post 2', 
+#         'content': 'Second post content', 
+#         'date_posted':'August 28, 2020'
+#     }
+# ]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     #Pass data to the render.
     return render(request, 'blog/home.html', context)
