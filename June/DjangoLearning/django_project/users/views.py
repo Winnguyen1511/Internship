b8f5165059e87f6d9cd871c8d962122af7b80263
Winnguyen <khoanguyen1507vn@gmail.com>
2020-07-01 09:51:03 +0700

    update

--- June/DjangoLearning/django_project/users/views.py
+++ June/DjangoLearning/django_project/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def register(request):
@@ -14,7 +14,11 @@ def register(request):
             #########################################
             username = form.cleaned_data['username']
             messages.success(request, f'Account created for {username}.')
-            return redirect('blog-home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request,'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
